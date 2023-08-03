26c3320a3c73329abe32667f893413f29c5eb200
Jorge Fonseca <jorgefonseca503@gmail.com>
2023-08-03 11:27:50 -0600

    esta es la lista de contactos en JavaScript

---
+++ contactos.js
@@ -0,0 +1,27 @@
+let listaDeContactos = [
+    "Mariana Morales",
+    "Jorge Fonseca",
+    "Andrea Ortega"
+];
+console.log(listaDeContactos)
+
+function agregarContacto (listaDeContactos){
+    listaDeContactos.push ("Alicia Espinal")
+};
+
+function borrarContacto (listaDeContactos){
+    const indice = listaDeContactos.indexOf(listaDeContactos);
+    if (indice !== -1) {
+        listaDeContactos.splice(indice-1);
+    }
+};
+
+function imprimirContactos() {
+    console.log("Lista de contactos:");
+    listaDeContactos.forEach((contacto, indice) => {
+      console.log(`${indice + 1}. ${contacto}`);
+    });
+  }
+  
+
+
